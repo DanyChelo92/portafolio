c9db01e4a2a6e48c13dbd8fc230772d90c8f7032
DanyChelo92 <danychelo92@gmail.com>
2023-02-22 00:54:32 -0300

    proyecto2 commit

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -93,10 +93,13 @@ DATABASES = {
         )
     }
 """
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/path/to/my.cnf',
+        },
     }
 }
 
